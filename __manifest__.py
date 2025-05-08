e30314a3dd55966da85a6227d46c8672ad7159c5
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-08 17:07:00 +0700

    Update module

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,15 @@
 {
     "name": "General Ledger Search Memory",
     "version": "1.0",
+    "summary": "Persist search query in General Ledger report",
     "author": "PT Lintang Utama Infotek",
+    "license": "LGPL-3",
     "depends": ["account_reports"],
-    # "assets": {
-    #     "web.assets_backend": [
-    #         "dpm_gl/static/src/js/general_ledger_search_cache.js",
-    #     ],
-    # },
-    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "dpm_gl/static/src/js/general_ledger_search_cache.js",
+        ],
+    },
     "installable": True,
     "application": False,
 }
