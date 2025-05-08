dece24b79d68fbc849732ce4ae03e00e59b0ffb3
nurdiansyahkudo <nurdiansyah192@gmail.com>
2025-05-08 16:59:30 +0700

    Update module

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,11 @@
     "version": "1.0",
     "author": "PT Lintang Utama Infotek",
     "depends": ["account_reports"],
-    "assets": {
-        "web.assets_backend": [
-            "dpm_gl/static/src/js/general_ledger_search_cache.js",
-        ],
-    },
+    # "assets": {
+    #     "web.assets_backend": [
+    #         "dpm_gl/static/src/js/general_ledger_search_cache.js",
+    #     ],
+    # },
     "data": [],
     "installable": True,
     "application": False,
